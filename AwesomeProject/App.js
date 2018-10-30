2c1fe8ca919506a33b379f794639512e62d12ba4
Leticia Monteiro <leticiamonteiro.design@gmail.com>
2018-10-30 20:23:21 -0300

    create a button component with animation

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,71 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View, Animated, TouchableWithoutFeedback } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import Button from './src/components/Button';
 
-export default class App extends React.Component {
+import styles from './src/styles/styles';
 
+export default class componentName extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      animation: new Animated.Value(1),
-    }
-  }
-
-  startAnimation = () => {
-    Animated.timing(this.state.animation, {
-      toValue: 2,
-      duration: 1500
-    }).start(() => {
-      Animated.timing(this.state.animation, {
-        toValue: 0,
-        duration: 1500,
-      }).start(() => {
-        Animated.timing(this.state.animation, {
-          toValue: 1,
-          duration: 1500,
-        }).start();
-      })
-    });
+    };
   }
 
   render() {
-
-    // Style dinamico
-    const animatedStyles = {
-      // opacity: this.state.animation
-      transform: [
-        {
-          // translateY: this.state.animation,
-          scale: this.state.animation,
-          // scaleX: this.state.animation,
-        }
-      ]
-    }
-    
+    const { container } = styles;
     return (
-      <View style={styles.container}>
-      <Text>Teste</Text>
-        <TouchableWithoutFeedback onPress={this.startAnimation}>
-
-        <Animated.View style={[styles.box, animatedStyles]} >
-        <Text>Vai ficar tudo bem</Text>
-        </Animated.View>
-
-        </TouchableWithoutFeedback>
+      <View style={container}>
+      <Text>Teste button</Text>
+        <Button textBtn="Entrar"/>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 150,
-    height: 150,
-    backgroundColor: 'tomato',
-  }
-});
